c39c488c3faaba20092012dde43e6a609f641807
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2019-04-19 00:47:37 -0500

    cykTable.h : mejorando importaciones, comentarios y función toString.

--- cykTable.h
+++ cykTable.h
@@ -1,20 +1,22 @@
-#include <algorithm>
-#include <fstream>
-#include <iostream>
 #include <iterator>
 #include <map>
 #include <unordered_set>
-#include <stdlib.h>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+// Clase tabla CYK, permite abstraer la tabla de posibles derivaciones para la
+// ejecución del algoritmo CYK.
 class CYKTable {
 private:
+    // La tabla CYK, implementada como un vector multidimensional, cuyos
+    // elementos son conjuntos sin orden.
     vector< vector<unordered_set <string> > >  table;
 
 public:
+    // Constructor de la tabla CYK. Recibe una cadena a partir de la cual se
+    // inicializa la tabla con conjuntos vacíos.
     CYKTable (string input){
         vector< vector<unordered_set <string> > > newTable(input.length());
         for (int i = 0; i < input.length(); i++) {
@@ -24,6 +26,8 @@ public:
         table = newTable;
     }
 
+    // Función que determina si una cadena figura en el conjunto subyaciente a
+    // cierta entrada de la tabla.
     bool isInEntry (string str, int i, int j){
         unordered_set <string> stringSet = table[i-1][j];
         if (stringSet.find(str) != stringSet.end())
@@ -32,32 +36,33 @@ public:
             return false;
     }
 
-    string getEntry (int i, int j){
-        string entry;
-        unordered_set <string> stringSet = table[i-1][j];
-
-        unordered_set<string>::iterator it;
-        for (it = stringSet.begin(); it != stringSet.end(); ++it) {
-            entry += *it + " : ";
-        }
-        return entry;
-    }
-
+    // Función que añade una cadena figura al conjunto subyaciente a
+    // cierta entrada de la tabla.
     void addToEntry (string str, int i, int j){
         table[i-1][j].insert(str);
     }
 
+    // Función que reegresa la representación en cadena de la tabla CYK.
     string toString(){
         string str;
         unordered_set<string>::iterator it;
         for (auto vec : table){
+            str += "[";
             for(auto stringSet : vec){
-                str += "| ";
-                for (it = stringSet.begin(); it != stringSet.end(); ++it) {
-                    str += *it + " ";
+                if (stringSet.empty()) {
+                    str += " Ø , ";
+                } else {
+                    str += "[";
+                    for (it = stringSet.begin(); it != stringSet.end(); ++it)
+                        str += *it + ", ";
+                    str.pop_back();
+                    str.pop_back();
+                    str += "], ";
                 }
             }
-            str += "| \n";
+            str.pop_back();
+            str.pop_back();
+            str += "]\n";
         }
         return str;
     }
